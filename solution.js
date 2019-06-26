1e63d6ed0cfda39f3823e8133f79d7a15e0b4e7a
Han Xu <hanxu27@gmail.com>
2019-06-26 09:34:58 -0400

    done

--- solution.js
+++ solution.js
@@ -1,1 +1,23 @@
+const data = [
+  [2, 3, 7, 8],
+  [0, 1, 3],
+  [9, 2, 8, 3]
+]
 
+function findSum(data) {
+  return data.map(row => {
+    let big = -Infinity
+    let small = Infinity
+    row.forEach(num => {
+      if (num > big) big = num
+      if (num < small) small = num
+    })
+    return (big - small)
+  })
+}
+
+function checkSum(data) {
+  return findSum(data).reduce(add)
+}
+
+const add = (a, b) => a + b
